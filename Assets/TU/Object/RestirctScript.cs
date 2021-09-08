d0c68beb054d4cff3348e85cb4a0b8fcdbaef837
TAKU334 <86240684+TAKU334@users.noreply.github.com>
2021-09-09 01:40:21 +0900

    summary
    UIシーン作成

---
+++ Assets/TU/Object/RestirctScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestirctScript : MonoBehaviour
+{
+    // カウントダウン
+    public float countdown = 10.0f;
+
+
+    //時間を表示するText型の変数
+    public Text timeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントダウンする
+        countdown -= Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1") ;
+
+        //countdownが0以下になったとき
+        if (countdown <= 0)
+        {
+            timeText.text = "終了！";
+        }
+
+    }
+}
