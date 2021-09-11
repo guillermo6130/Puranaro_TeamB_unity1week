be3f179a64106fefa2deb12bf0b771055276d5df
guillermo6130 <ogs613@i.softbank.jp>
2021-09-11 13:29:32 +0900

    update

--- Assets/Guillermo/Scripts/FortuneGauge.cs
+++ Assets/Guillermo/Scripts/FortuneGauge.cs
@@ -1,35 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FortuneGauge : MonoBehaviour
 {
     [SerializeField] GameObject Handle;
     HandleController HandleController;
+    [SerializeField] GameObject creation;
+    CreateFortuneBall createFortuneBall;
 
+    Slider slider;
     float FortuneValue=0;
     float preRotateNum=0;
 
     [SerializeField]float maxFortuneValue = 100;
+    bool FortuneBurst=false;
+    [SerializeField]float FortuneBurstValue = 3f;
+
+    [SerializeField]float RotateVar = 4;
 
     // Start is called before the first frame update
     void Start()
     {
         HandleController = Handle.GetComponent<HandleController>();
+        slider = this.GetComponent<Slider>();
+
+        createFortuneBall = creation.GetComponent<CreateFortuneBall>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        FortuneIncByRotateNum();
+        if (FortuneValue < maxFortuneValue&&FortuneBurst==false)
+        {
+            FortuneIncByRotateNum();
+        }
+        else if (FortuneValue >= maxFortuneValue)
+        {
+            FortuneBurst = true;
+            createFortuneBall.set100Percent();
+        }
+        if (FortuneBurst == true)
+        {
+            FortuneValue -= FortuneBurstValue;
+            if (FortuneValue <= 0)
+            {
+                FortuneBurst = false;
+                FortuneValue = 0;
+                createFortuneBall.returnPercent();
+            }
+        }
         //Debug.Log(FortuneValue);
+        slider.value = FortuneValue / maxFortuneValue;
     }
 
     public void FortuneIncByRotateNum()
     {
         float IncRotateNum = 0;
         IncRotateNum = HandleController.getRotateNum();
-        FortuneValue += IncRotateNum-preRotateNum;
+        FortuneValue += (IncRotateNum-preRotateNum)*RotateVar;
         preRotateNum = HandleController.getRotateNum();
         //Debug.Log(FortuneValue);
 
@@ -40,6 +71,8 @@ public class FortuneGauge : MonoBehaviour
     {
         FortuneValue += point;
     }
+
+   
     
     
     
