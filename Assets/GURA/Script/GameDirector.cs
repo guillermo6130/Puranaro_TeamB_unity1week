c67c149ae721fed0067e352e2da960f222da4553
IshiharaGura <yukkurigurani@gmail.com>
2021-09-12 02:39:48 +0900

    GameDirectorスクリプトを修正
    Bool関数でResultSceneへの遷移トリガーになるようにしました。
    関数を参照し、ゲーム終了時にtrueにしてください

--- Assets/GURA/Script/GameDirector.cs
+++ Assets/GURA/Script/GameDirector.cs
@@ -6,22 +6,31 @@ using UnityEngine.SceneManagement;
 public class GameDirector : MonoBehaviour
 {
     [SerializeField] GameObject FadeIn;
+    public bool ResultTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ResultTrigger = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ResultTrigger)
+        {
+            FadeIn.GetComponent<FadeIn>().Trigger = true;
+            //FadeInオブジェクトをヒエラルキー（Canvas)の一番下に
+            FadeIn.transform.SetAsLastSibling();
+        }
+
         //FadeIn完了時にResultSceneへ遷移
         if (FadeIn.GetComponent<FadeIn>().alfa_In >= 1)
         {
             //Triggerをfalseにする
             //シーン遷移
             FadeIn.GetComponent<FadeIn>().Trigger = false;
+            ResultTrigger = false;
             SceneManager.LoadScene("ResultScene");
         }
     }
@@ -29,9 +38,6 @@ public class GameDirector : MonoBehaviour
     //ResultScene遷移のテストボタン用
     public void ResultOnClick()
     {
-        FadeIn.GetComponent<FadeIn>().Trigger = true;
-        //FadeInオブジェクトをヒエラルキー（Canvas)の一番下に
-        FadeIn.transform.SetAsLastSibling();
+        ResultTrigger = true;
     }
-
 }
